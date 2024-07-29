052fcf75f33fe42d7b3ec9d1c376000e087c7270
Yuliia Hots <goculiya@gmail.com>
2024-07-29 15:14:15 +0200

    imagePicker

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import * as ImagePicker from "expo-image-picker";
+import { s } from "./App.style";
+import { useState } from "react";
 
 export default function App() {
+  const [imageURIList, setImageURIList] = useState([]);
+  async function pickImageAsync() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (result.canceled) {
+      alert("Aucune image selectionnée");
+    } else {
+      // console.log(result.assets[0].uri);
+      setImageURIList([...imageURIList, result.assets[0].uri]);
+    }
+  }
+  console.log("***", imageURIList);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text style={s.title}>My favorite pictures</Text>
+        <View style={s.body}>
+          <ScrollView>
+            {imageURIList.map((imageURI, i) => (
+              <Image
+                style={{ height: 300, marginVertical: 30, width: "100%" }}
+                key={imageURI + 1}
+                source={{ uri: imageURI }}
+              />
+            ))}
+          </ScrollView>
+        </View>
+        <View style={s.footer}>
+          <TouchableOpacity style={s.btn} onPress={pickImageAsync}>
+            <Text style={s.btnTxt}>Add picture</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
